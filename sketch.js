067f48a5eedf16af3a7bd4d488f296b6ee2fead2
pranayjangid <74404469+pranayjangid@users.noreply.github.com>
2021-01-04 16:24:56 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
